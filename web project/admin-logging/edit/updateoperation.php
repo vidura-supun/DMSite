32a5d1c25b76ef9d47eeda70693998b5148a6041
AkilaPrabash <akilaprabash96@gmail.com>
2017-11-21 16:07:55 +0530

    Add files via upload

---
+++ web project/admin-logging/edit/updateoperation.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include('../connection.php');
+$connection = new createConnection();
+$connection_ref = $connection->connectToDatabase();
+
+
+
+$selected_table_name=$_SESSION["tableName"];
+
+
+   $wrecord = $_GET['wrecord'];
+   $wcolumn = $_GET['wcolumn'];
+   $wpm = $_GET['wvalue'];
+
+//to prevent sqli
+   $wrecord = mysqli_real_escape_string($connection_ref, $wrecord);
+   $wcolumn = mysqli_real_escape_string($connection_ref, $wcolumn);
+   $wpm = mysqli_real_escape_string($connection_ref, $wpm);
+
+$allsel="select * from ".$selected_table_name;
+$res = mysqli_query($connection_ref, $allsel);
+
+
+
+$finfo = mysqli_fetch_field_direct( $res, $wcolumn);
+
+$fld_name = $finfo->name;
+$sql = "UPDATE ".$selected_table_name." SET ".$fld_name."='".$wpm."' WHERE id=".$wrecord;
+
+
+$a=mysqli_query($connection_ref, $sql);
+?>
