805a991b5ef739b494e05e9c700007aef69fde95
vidura-supun <vidurasupun@gmail.com>
2017-12-05 09:57:48 +0530

    new

--- web project/admin-logging/connection.php
+++ web project/admin-logging/connection.php
@@ -1,58 +1,14 @@
 <?php
 
-class createConnection
-{
-    var $host="localhost";
-    var $username="root";
-    Var $password="";
-    var $database="Vipath";
+$host="localhost";
+$username="root";
+$password="";
+$database="Vipath";
 
-    var $myconn;
+$myConn = mysqli_connect($host, $username, $password,$database);
 
-
-
-    function connectToDatabase()
-    {
-        $conn= mysqli_connect($this->host,$this->username,$this->password, $this->database);
-
-        if(!$conn)
-        {
-            die ("Connection error connecting to the database");
-        }
-
-        else
-        {
-
-            $this->myconn = $conn;
-
-
-
-        }
-
-        return $this->myconn;
-
-    }
-
-    function selectDatabase()
-    {
-        mysql_select_db($this->database);
-
-        if(mysql_error())
-        {
-
-            echo "Database not found".$this->database;
-
-        }
-
-    }
-
-    function closeConnection()
-    {
-        mysql_close($this->myconn);
-
-
-    }
-
+if (!$myConn) {
+    die("<p>Connection failed: " . mysqli_connect_error() . "<p>");
 }
 
-?>
+ ?>
