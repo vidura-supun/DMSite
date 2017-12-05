2695abead9b3918cb34d0092458d6aea11123261
vidura-supun <vidurasupun@gmail.com>
2017-12-05 10:44:37 +0530

    untangled

--- web project/web project/admin-logging/login/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['login_user']);
-	if(session_destroy())
-	{
-		header("Location:index.php"); 
-	}
-?>
--- web project/web project/admin-logging/login/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if(!isset($_SESSION['login_user']))
-{
-	header("location:login/index.php");
-}
-?>
