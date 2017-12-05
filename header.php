4fb2534c80fae4479fff5939bc3cd1212235a70a
AkilaPrabash <akilaprabash96@gmail.com>
2017-12-04 20:47:54 -1200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+  	   	<title>ADMIN Panel</title>
+
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<style>
+		html,body{width:100%;height:100%;}
+                .fix_nav{margin-top:10%;}
+		.win_by_vipath{padding:3em 0em;text-align: center;margin:1.5em 1em;text-shadow:5px 5px 10px black;font-size:18px; background-color: grey;}
+.wrapper{line-height:12;}
+a:hover{text-decoration:none;}
+
+	</style>
+
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top" style="background:#eeefc6">
+
+
+      		</button>
+	      	<a class="navbar-brand" href="http://nsbm.lk/">
+          		<span class="glyphicon glyphicon-globe" style="font-size:3em"></span>
+      		</a>
+		<a class="navbar-brand" href="#">
+          		<span><b style="font-size:12px;letter-spacing:5px"> whats happening in the country at the moment </b><br>ADMIN AT VIPATH</span>
+      		</a>
+
+    	<div class="collapse navbar-collapse" id="myNavbar">
+	      	<ul class="nav navbar-nav navbar-right">
+        		<li ><p style="margin-top:10%;">ADMINSTRATOR LOGGED IN<br><b style="font-size:12px;letter-spacing:5px">vipath admin backend</b></p></li>
+        		<li><a href="login/logout.php"><span class="glyphicon glyphicon-off" style="font-size:2.5em"></span></a></li>
+      		</ul>
+    	</div>
+
+      <div align="right" >
+      <form action="create/index.php">
+        <input type="submit" value="create new user">
+      </form>
+      </div>
+</nav>
+</body>
+</html>
