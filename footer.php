a98540694a5750cb8d8d2fbabc84ede73bc1bf8f
HiranErandika <hiranerandika07@gmail.com>
2017-12-07 14:59:28 +0530

    final footer

--- footer.php
+++ footer.php
@@ -1,4 +1,38 @@
-
+<link href="css/bootstrap.min.css" rel="stylesheet">
+    
+        <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
+        <link href="css/custom.css" rel="stylesheet">
+    
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    
+        <!-- Custom Fonts from Google -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+            rel='stylesheet' type='text/css'>
+  
+    <footer class="page-footer">
+    
+    
+            <div class="contact">
+                <div class="container">
+                    <h2 class="section-heading">Contact Us</h2>
+                    <p>
+                        <span class="glyphicon glyphicon-earphone"></span>
+                        <br> +94 112 456 7890</p>
+                    <p>
+                        <span class="glyphicon glyphicon-earphone"></span>
+                        <br> +94 112 976 7890</p>
+                    <p>
+                        <span class="glyphicon glyphicon-envelope"></span>
+                        <br> info@Vipath.lk</p>
+                </div>
+            </div>
+    
+    
 
 <footer class="footer" text align = "center" style="background-color: powderblue;font-family: latto"; >
       <div class="container">
